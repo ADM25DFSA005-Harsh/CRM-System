955ea434e5b7e04cb6642b96d0cc828ad3ac02b9
KPratyusha872 <k.pratyusha@cognizant.com>
2025-10-21 14:31:47 +0530

    Update CustomerSupportController.cs

--- CRMSystem/Controllers/CustomerSupportController.cs
+++ CRMSystem/Controllers/CustomerSupportController.cs
@@ -1,4 +1,128 @@
-﻿using CRMSystem.Data;
+//using CRMSystem.Data;
+//using Microsoft.AspNetCore.Mvc;
+//using CRMSystem.Models;
+//using CRMProject.Models;
+
+//namespace CRMProject.Controllers
+//{
+//    public class CustomerSupportController : Controller
+//    {
+//        CrmDbContext supportTicketDb;
+
+//        public CustomerSupportController(CrmDbContext _supportTicketDb)
+//        {
+//            supportTicketDb = _supportTicketDb;
+
+//        }
+
+//        [HttpGet]
+//        public IActionResult DisplayTicketsInput()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public IActionResult DisplayTicketsInput(SupportTicket data)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return View(data);
+//            }
+
+//            supportTicketDb.SupportTicket.Add(data);
+//            supportTicketDb.SaveChanges();
+
+//            return RedirectToAction("DisplayInTableForm");
+//        }
+
+//        [HttpGet]
+//        public IActionResult DisplayInTableForm(string statusFilter)
+//        {
+//            var tickets = string.IsNullOrEmpty(statusFilter)
+//                ? supportTicketDb.SupportTicket.ToList()
+//                : supportTicketDb.SupportTicket.Where(t => t.Status == statusFilter).ToList();
+
+//            return View(tickets);
+//        }
+
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public IActionResult DeleteByCustomerId(int customerId)
+//        {
+//            var ticket = supportTicketDb.SupportTicket.FirstOrDefault(t => t.CustomerID == customerId);
+//            if (ticket != null)
+//            {
+//                supportTicketDb.SupportTicket.Remove(ticket);
+//                supportTicketDb.SaveChanges();
+//            }
+//            return RedirectToAction("DisplayInTableForm");
+//        }
+
+//        [HttpGet]
+//        public IActionResult EditByCustomerId(int customerId)
+//        {
+//            var ticket = supportTicketDb.SupportTicket.FirstOrDefault(t => t.CustomerID == customerId);
+//            if (ticket == null)
+//            {
+//                return NotFound();
+//            }
+//            return View(ticket);
+//        }
+
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public IActionResult EditByCustomerId(SupportTicket updatedTicket)
+//        {
+//            var existingTicket = supportTicketDb.SupportTicket.FirstOrDefault(t => t.CustomerID == updatedTicket.CustomerID);
+//            if (existingTicket == null)
+//            {
+//                return NotFound();
+//            }
+
+//            existingTicket.Issue = updatedTicket.Issue;
+//            existingTicket.AssignedAgent = updatedTicket.AssignedAgent;
+//            existingTicket.Status = updatedTicket.Status;
+
+//            supportTicketDb.SaveChanges();
+//            return RedirectToAction("DisplayInTableForm");
+//        }
+
+//        [HttpGet]
+//        public IActionResult ViewTicket(int customerId)
+//        {
+//            var ticket = supportTicketDb.SupportTicket.FirstOrDefault(t => t.CustomerID == customerId);
+//            if (ticket == null)
+//            {
+//                return NotFound();
+//            }
+//            return View(ticket);
+//        }
+
+//        [HttpGet]
+//        public IActionResult DisplayInTableForm(int? SearchById, string? statusFilter)
+//        {
+//            var tickets = supportTicketDb.SupportTicket.AsQueryable();
+
+//            if (SearchById.HasValue)
+//            {
+//                tickets = tickets.Where(t => t.CustomerID == SearchById.Value);
+//            }
+
+//            if (!string.IsNullOrEmpty(statusFilter))
+//            {
+//                tickets = tickets.Where(t => t.Status == statusFilter);
+//            }
+
+//            return View(tickets.ToList());
+//        }
+
+
+
+//    }
+//}
+
+using CRMSystem.Data;
 using Microsoft.AspNetCore.Mvc;
 using CRMSystem.Models;
 using CRMProject.Models;
@@ -7,20 +131,21 @@ namespace CRMProject.Controllers
 {
     public class CustomerSupportController : Controller
     {
-        CrmDbContext supportTicketDb;
+        private readonly CrmDbContext supportTicketDb;
 
         public CustomerSupportController(CrmDbContext _supportTicketDb)
         {
             supportTicketDb = _supportTicketDb;
-
         }
 
+        // GET: Ticket input form
         [HttpGet]
         public IActionResult DisplayTicketsInput()
         {
             return View();
         }
 
+        // POST: Save new ticket
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult DisplayTicketsInput(SupportTicket data)
@@ -36,16 +161,26 @@ namespace CRMProject.Controllers
             return RedirectToAction("DisplayInTableForm");
         }
 
+        // GET: Display filtered tickets (by CustomerID or Status or both)
         [HttpGet]
-        public IActionResult DisplayInTableForm(string statusFilter)
+        public IActionResult DisplayInTableForm(int? SearchById, string? statusFilter)
         {
-            var tickets = string.IsNullOrEmpty(statusFilter)
-                ? supportTicketDb.SupportTicket.ToList()
-                : supportTicketDb.SupportTicket.Where(t => t.Status == statusFilter).ToList();
+            var tickets = supportTicketDb.SupportTicket.AsQueryable();
 
-            return View(tickets);
+            if (SearchById.HasValue)
+            {
+                tickets = tickets.Where(t => t.CustomerID == SearchById.Value);
+            }
+
+            if (!string.IsNullOrEmpty(statusFilter))
+            {
+                tickets = tickets.Where(t => t.Status == statusFilter);
+            }
+
+            return View(tickets.ToList());
         }
-        
+
+        // POST: Delete ticket by CustomerID
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult DeleteByCustomerId(int customerId)
@@ -56,9 +191,11 @@ namespace CRMProject.Controllers
                 supportTicketDb.SupportTicket.Remove(ticket);
                 supportTicketDb.SaveChanges();
             }
+
             return RedirectToAction("DisplayInTableForm");
         }
 
+        // GET: Edit ticket by CustomerID
         [HttpGet]
         public IActionResult EditByCustomerId(int customerId)
         {
@@ -67,9 +204,11 @@ namespace CRMProject.Controllers
             {
                 return NotFound();
             }
+
             return View(ticket);
         }
 
+        // POST: Save edited ticket
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult EditByCustomerId(SupportTicket updatedTicket)
@@ -81,6 +220,7 @@ namespace CRMProject.Controllers
             }
 
             existingTicket.Issue = updatedTicket.Issue;
+            existingTicket.IssueDescription = updatedTicket.IssueDescription;
             existingTicket.AssignedAgent = updatedTicket.AssignedAgent;
             existingTicket.Status = updatedTicket.Status;
 
@@ -88,6 +228,7 @@ namespace CRMProject.Controllers
             return RedirectToAction("DisplayInTableForm");
         }
 
+        // GET: View ticket details by CustomerID
         [HttpGet]
         public IActionResult ViewTicket(int customerId)
         {
@@ -96,8 +237,116 @@ namespace CRMProject.Controllers
             {
                 return NotFound();
             }
+
             return View(ticket);
         }
-
     }
 }
+
+
+
+
+
+// ﻿using CRMSystem.Data;
+// using Microsoft.AspNetCore.Mvc;
+// using CRMSystem.Models;
+// using CRMProject.Models;
+
+// namespace CRMProject.Controllers
+// {
+//     public class CustomerSupportController : Controller
+//     {
+//         CrmDbContext supportTicketDb;
+
+//         public CustomerSupportController(CrmDbContext _supportTicketDb)
+//         {
+//             supportTicketDb = _supportTicketDb;
+
+//         }
+
+//         [HttpGet]
+//         public IActionResult DisplayTicketsInput()
+//         {
+//             return View();
+//         }
+
+//         [HttpPost]
+//         [ValidateAntiForgeryToken]
+//         public IActionResult DisplayTicketsInput(SupportTicket data)
+//         {
+//             if (!ModelState.IsValid)
+//             {
+//                 return View(data);
+//             }
+
+//             supportTicketDb.SupportTicket.Add(data);
+//             supportTicketDb.SaveChanges();
+
+//             return RedirectToAction("DisplayInTableForm");
+//         }
+
+//         [HttpGet]
+//         public IActionResult DisplayInTableForm(string statusFilter)
+//         {
+//             var tickets = string.IsNullOrEmpty(statusFilter)
+//                 ? supportTicketDb.SupportTicket.ToList()
+//                 : supportTicketDb.SupportTicket.Where(t => t.Status == statusFilter).ToList();
+
+//             return View(tickets);
+//         }
+        
+//         [HttpPost]
+//         [ValidateAntiForgeryToken]
+//         public IActionResult DeleteByCustomerId(int customerId)
+//         {
+//             var ticket = supportTicketDb.SupportTicket.FirstOrDefault(t => t.CustomerID == customerId);
+//             if (ticket != null)
+//             {
+//                 supportTicketDb.SupportTicket.Remove(ticket);
+//                 supportTicketDb.SaveChanges();
+//             }
+//             return RedirectToAction("DisplayInTableForm");
+//         }
+
+//         [HttpGet]
+//         public IActionResult EditByCustomerId(int customerId)
+//         {
+//             var ticket = supportTicketDb.SupportTicket.FirstOrDefault(t => t.CustomerID == customerId);
+//             if (ticket == null)
+//             {
+//                 return NotFound();
+//             }
+//             return View(ticket);
+//         }
+
+//         [HttpPost]
+//         [ValidateAntiForgeryToken]
+//         public IActionResult EditByCustomerId(SupportTicket updatedTicket)
+//         {
+//             var existingTicket = supportTicketDb.SupportTicket.FirstOrDefault(t => t.CustomerID == updatedTicket.CustomerID);
+//             if (existingTicket == null)
+//             {
+//                 return NotFound();
+//             }
+
+//             existingTicket.Issue = updatedTicket.Issue;
+//             existingTicket.AssignedAgent = updatedTicket.AssignedAgent;
+//             existingTicket.Status = updatedTicket.Status;
+
+//             supportTicketDb.SaveChanges();
+//             return RedirectToAction("DisplayInTableForm");
+//         }
+
+//         [HttpGet]
+//         public IActionResult ViewTicket(int customerId)
+//         {
+//             var ticket = supportTicketDb.SupportTicket.FirstOrDefault(t => t.CustomerID == customerId);
+//             if (ticket == null)
+//             {
+//                 return NotFound();
+//             }
+//             return View(ticket);
+//         }
+
+//     }
+// }
